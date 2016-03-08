93fe51ffd058a0bdbd757c50cd801ce3da3fe978
Insights Service EC2 User <ec2@InsightsService>
2016-03-08 14:26:05 +0000

    Instrumented by Insights Service for uploading the artifacts

--- df-build.sh
+++ df-build.sh
@@ -3,8 +3,9 @@ mvn dependency:copy-dependencies || { echo "Error: maven dependency:copy-depende
 echo "Cleaning up existing zip if any..."
 rm -f libs.zip || { echo "Error: Cleaning lib zip"; exit 1; }
 rm -f build.zip || { echo "Error: Cleaning binaries zip"; exit 1; }
+rm -f build.zip || { echo "Error: Cleaning binaries zip"; exit 1; }
 echo "Zipping artifacts begin..."
 zip libs.zip $(git ls-files -o | grep -e target/dependency/.*jar) || { echo "Error: Zipping libs failed"; exit 1; }
 zip build.zip $(git ls-files -o | grep -e target/classes/.*class) || { echo "Error: Zipping binaries failed"; exit 1; }
 echo "Zipping artifacts DONE!!!"
-# Modified by Insights Service at 2016-03-05 16:41:52.167886
+# Modified by Insights Service at 2016-03-08 14:26:04.454065
